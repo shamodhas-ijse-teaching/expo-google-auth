1bbb65bdd350f37254eeb2b11031eb34a7d3558b
shamodhas <shamodha7@gmail.com>
2026-02-02 11:04:48 +0530

    chore: update react-native-safe-area-context version and add tailwind configuration file

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }]],
-    plugins: [
-      'nativewind/babel', 
-      'react-native-reanimated/plugin' 
-    ]
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel'
+    ],
+    plugins: ['nativewind/babel', 'react-native-reanimated/plugin']
   }
 }
